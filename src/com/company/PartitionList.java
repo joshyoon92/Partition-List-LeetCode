0d4babb0ed75bbedd27f0333476baaedc47f96fb
Joshua Yoon <joshyoon92@gmail.com>
2021-04-14 09:31:17 +0900

    Initial commit

---
+++ src/com/company/PartitionList.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class PartitionList {
+    public ListNode partitionList(ListNode head, int x){
+        ListNode b_head = new ListNode(0);
+        ListNode b = b_head;
+        ListNode a_head = new ListNode(0);
+        ListNode a = a_head;
+
+        while (head != null){
+            if (head.val < x){
+                b.next = head;
+                b = b.next;
+            } else {
+                a.next = head;
+                a = a.next;
+            }
+            head = head.next;
+        }
+        a.next = null;
+        b.next = a_head.next;
+        return b_head.next;
+    }
+}
